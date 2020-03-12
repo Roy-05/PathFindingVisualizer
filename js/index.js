c99998fb10c0d488d48fae27a4e7a7ccf16faee5
Saket Roy <saket.roy@hotmail.com>
2020-03-12 10:24:07 +0530

    Add ids to each node elem

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ table = document.getElementById("node-table")
 
 for(let i =0; i<15; i++){
     table_row = document.createElement("tr")
+    table_row.id = `row-${i}`
     table.appendChild(table_row)
     for(let j=0; j<20; j++){
         table_column = document.createElement("td")
+        table_column.id = `row-${i}-col-${j}`
         table_row.appendChild(table_column)
     }
 }
